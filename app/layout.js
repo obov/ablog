15137899d75e1cad74365e33c5d934d0184aca64
obov <59566239+obov@users.noreply.github.com>
2024-12-29 18:05:19 +0900

    feat: emoji on home page

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,22 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const emojiSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64">
+      <text x="50%" y="50%" font-size="48" text-anchor="middle" dominant-baseline="central">
+        📌
+      </text>
+    </svg>
+  `;
+const svgBase64 = Buffer.from(emojiSvg).toString('base64');
+const svgUrl = `data:image/svg+xml;base64,${svgBase64}`;
+
 export const metadata = {
   title: 'Notion Next.js blog',
   description: 'Notion Next.js blog',
+  icons: {
+    icon: svgUrl,
+  },
 };
 
 export default function RootLayout({ children }) {
